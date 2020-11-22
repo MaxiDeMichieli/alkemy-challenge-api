2b08d4ea7dbfd41966c7a5ab561a3f07175fd76b
MaxiDeMichieli <maxidemichieli@gmail.com>
2020-11-22 00:00:50 -0300

    Configura nodemailer

---
+++ src/functions/nodemailerTransporter.js
@@ -0,0 +1,16 @@
+const nodemailer = require('nodemailer');
+const process = require('process');
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.MAIL_USER,
+        clientId: process.env.MAIL_CLIENT_ID,
+        clientSecret: process.env.MAIL_CLIENT_SECRET,
+        refreshToken: process.env.MAIL_REFRESH_TOKEN,
+        accessToken: process.env.MAIL_ACCESS_TOKEN
+    }
+});
+
+module.exports = transporter

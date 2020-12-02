6452d0e9dfd830b2b46f056cb5e846f06778bc69
MaxiDeMichieli <maxidemichieli@gmail.com>
2020-12-02 15:10:10 -0300

    Configura conexion a db

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "b8734e5f0b1a49",
+    "password": "bf5a4c38",
+    "database": "heroku_7dd43b6feddf5ef",
+    "host": "us-cdbr-east-02.cleardb.com",
     "dialect": "mysql"
   }
 }
